6804ec5b0f4fafc40275fd72ff6f60b18fb580d4
Julian Hüpppauff <julian@hueppauff.info>
2019-10-22 21:54:40 +0200

    added loading animation

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 import GetBuildNumber from './components/BuildNumberService'
+//@ts-ignore
+import Loader from 'react-loader-spinner'
 
 const App: React.FC<{}> = () => {
   const service = GetBuildNumber();
 
   return (
     <div>
-      {service.status === 'loading' && <div>Loading...</div>}
+      {service.status === 'loading' && <div className="App"><header className="App-header"><Loader
+         type="CradleLoader"
+         color="#00BFFF"
+         height={100}
+         width={100}
+         timeout={3000} />
+         </header></div>}
       {service.status === 'loaded' && (
         <div className="App">
         <header className="App-header">
