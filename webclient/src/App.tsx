a41fbb54dbd32d0a2498fa8b5a98f14615496c37
Julian Hüpppauff <julian@hueppauff.info>
2019-10-23 17:55:26 +0200

    reformating, cleanup

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -9,24 +9,28 @@ const App: React.FC<{}> = () => {
 
   return (
     <div>
-      {service.status === 'loading' && <div className="App"><header className="App-header"><Loader
-         type="CradleLoader"
-         color="#00BFFF"
-         height={100}
-         width={100}
-         timeout={3000} />
-         </header></div>}
-      {service.status === 'loaded' && (
+      {service.status === 'loading' && 
+      <div className="App">
+        <header className="App-header">
+          <p>Warming up the Server</p>
+          <Loader type="CradleLoader" color="#00BFFF" height={100} width={100} timeout={3000} />
+        </header>
+      </div>}
+      {service.status === 'loaded' && 
         <div className="App">
         <header className="App-header">
           <p>The Build Number for today is :</p>
           <b>{service.payload.Version}</b>
           </header>
         </div>
-      )}
-      {service.status === 'error' && (
-        <div>Error, the backend moved to the dark side.</div>
-      )}
+      }
+      {service.status === 'error' && 
+        <div className="App">
+          <header className="App-header">
+            <p>Oppps .. an Error occured, properly the backend moved to the dark side. :(</p>
+          </header>
+        </div>
+      }
     </div>
   );
 };
