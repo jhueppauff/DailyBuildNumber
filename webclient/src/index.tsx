a69b70cb432861d21fcc0248a77359d30d905bb4
Julian Hüpppauff <julian@hueppauff.info>
2019-10-23 17:53:25 +0200

    added application insights

--- webclient/src/index.tsx
+++ webclient/src/index.tsx
@@ -4,6 +4,14 @@ import './index.css';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import App from './App';
 
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const appInsights = new ApplicationInsights({ config: {
+  instrumentationKey: '3e07db04-1d1b-4cb8-a18b-6b7269643bec'
+  /* ...Other Configuration Options... */
+} });
+appInsights.loadAppInsights();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
