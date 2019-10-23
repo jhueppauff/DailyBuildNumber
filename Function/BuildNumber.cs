7aac0fb80adcaed14687aefb8a87ab255830ca1d
Julian Hüpppauff <julian@hueppauff.info>
2019-10-23 18:34:35 +0200

    Additional validation, changed visibility

--- Function/BuildNumber.cs
+++ Function/BuildNumber.cs
@@ -18,14 +18,18 @@ namespace DailyBuildNumber.Function
             {
                 throw new FormatException("Year can't contain more than 2 digits");
             }
+            else if (year.Length() < 0)
+            {
+                throw new FormatException("Year needs to be possitive");
+            }            
 
             this.Year = year;
             this.DayOfYear = dayOfYear;
         }
 
-        public int Year { get; set; }
+        private int Year { get; set; }
 
-        public int DayOfYear { get; set; }
+        private int DayOfYear { get; set; }
 
         public override string ToString() 
         {
